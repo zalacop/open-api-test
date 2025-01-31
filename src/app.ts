77683007d13d050a304a6b5165e3fd6ca42fcdda
zalacop <103320914+zalacop@users.noreply.github.com>
2025-01-31 15:22:36 +0100

    adds omitting password

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ const port = config.get<number>('port')
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(port, async () => {
     log.info (`App is running at http://localhost:${port}`);
 
--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import log from "../utils/logger";
 import { CreateUser } from "../service/user.service";
 import { CreateUserInput } from "../schema/user.schema";
+import { omit } from "lodash";
 
 export async function createUserHandler(
     req: Request<{}, {}, CreateUserInput["body"]>, 
@@ -9,9 +10,9 @@ export async function createUserHandler(
 ) {
     try{
         const user = await CreateUser(req.body);
-        return user;
+        return res.send(omit(user.toJSON(), "password"));
     } catch(error: any) {
         log.error(error);
         return res.status(409).send(error.message);
     }
-}
+} 
