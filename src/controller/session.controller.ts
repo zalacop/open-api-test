0a15ddac3d1fd36dc542461c3b5116adc97254bc
zalacop <103320914+zalacop@users.noreply.github.com>
2025-01-31 15:53:20 +0100

    adds validate password and start of creating a session

---
+++ src/controller/session.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { validatePassword } from "../service/user.service";
+
+export async function createUserSessionHandler(req: Request, res: Response) {
+
+    // validate the user's password
+    const user = await validatePassword(req.body);
+
+    if (!user) {
+        return res.status(401).send("Invalid email or password");
+    }
+
+    // create a session
+
+    // create an access token
+
+    // create a refresh token
+
+    // return access & refresh tokens
+}
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,13 +1,35 @@
 import { InferSchemaType } from "mongoose";
 import UserModel, { UserDocument } from "../models/user.models";
+import { omit } from "lodash";
 
 
 export async function CreateUser(
     input: InferSchemaType<Omit<UserDocument, "createdAt" | "updatedAt" | "comparePassword">>
 ) {
     try{
-        return await UserModel.create(input)
+        const user = await UserModel.create(input);
+
+        return omit(user.toJSON(), 'password');
     } catch(error: any) {
         throw new Error(error);
     }
+}
+
+export async function validatePassword({
+    email, password
+}: {
+    email: string;
+    password: string;
+}) {
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {{
+        return false;
+    }};
+
+    const isValid = await user.comparePassword(password);
+
+    if (!isValid) return false;
+
+    return omit(user.toJSON(), 'password');
 }
