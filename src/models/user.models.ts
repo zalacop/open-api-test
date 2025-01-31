d251aaf7b0383db259993b3eab10179b53ae0423
zalacop <103320914+zalacop@users.noreply.github.com>
2025-01-31 14:11:17 +0100

    adds hash password

--- src/models/user.models.ts
+++ src/models/user.models.ts
@@ -2,6 +2,14 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import config from "config";
 
+export interface UserDocument extends mongoose.Document {
+    email: string;
+    name: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
 const userSchema = new mongoose.Schema(
     {
         email: { type: String, required: true, unique: true },
@@ -12,6 +20,23 @@ const userSchema = new mongoose.Schema(
         timestamps: true
     }
 );
+
+userSchema.pre("save", async function (next: mongoose.CallbackWithoutResultAndOptionalError) {
+
+    let user = this as UserDocument;
+
+    if (!user.isModified("password")) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
+
+    const hash = await bcrypt.hashSync(user.password, salt);
+
+    user.password = hash;
+
+    return next();
+})
 
 const UserModel = mongoose.model("User", userSchema);
 
