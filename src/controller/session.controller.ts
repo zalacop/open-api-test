74400f931ac9497061a7ad1c09885c334b627b24
zalacop <103320914+zalacop@users.noreply.github.com>
2025-02-10 18:29:06 +0100

    debug

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -35,7 +35,7 @@ export async function createUserSessionHandler(req: Request, res: Response) {
 export async function getUserSessionsHandler(req: Request, res: Response) {
     const userId = res.locals.user._id;
 
-    const sessions = await findSessions({ user: userId, valid: false });
+    const sessions = await findSessions({ user: userId, valid: true });
 
     return res.send(sessions);
 }
