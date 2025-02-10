39dfb2437ad5d4d956f054bdf6b42ff4be2697b5
zalacop <103320914+zalacop@users.noreply.github.com>
2025-02-10 18:15:42 +0100

    adds get user sessions route

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import { Express, Request, Response } from "express";
 import { createUserHandler } from "./controller/user.controller";
 import validate from "./middleware/validateResource";
 import { createUserSchema } from "./schema/user.schema";
-import { createUserSessionHandler } from "./controller/session.controller";
+import { createUserSessionHandler, getUserSessionsHandler } from "./controller/session.controller";
 import { createSessionSchema } from "./schema/session.schema";
 
 function routes(app: Express) {
@@ -11,6 +11,8 @@ function routes(app: Express) {
     app.post("/api/users", validate(createUserSchema), createUserHandler);
 
     app.post("/api/sessions", validate(createSessionSchema), createUserSessionHandler);
+
+    app.get("/api/sessions", getUserSessionsHandler)
 }
 
 export default routes;
