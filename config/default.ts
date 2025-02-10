35274ec2f928d46703d5b16d7b43e3d9bb0e81b7
zalacop <103320914+zalacop@users.noreply.github.com>
2025-02-10 17:31:36 +0100

    adds access token

--- config/default.ts
+++ config/default.ts
@@ -2,4 +2,7 @@ export default {
     port: 1337,
     dbUri: "mongodb://localhost:27017/open-api-test",
     saltWorkFactor: 10,
+    accessTokenTtl: '15m',
+    refreshTokenTtl: '1y',
+    
 }
--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { validatePassword } from "../service/user.service";
 import { createSession } from "../service/session.service";
+import { signJwt } from "../utils/jwt.utils";
+import config from "config";
 
 export async function createUserSessionHandler(req: Request, res: Response) {
    
@@ -15,8 +17,10 @@ export async function createUserSessionHandler(req: Request, res: Response) {
     const session = await createSession(user._id, req.get("user-agent") || "");
 
      // create an access token
+     const accessToken = signJwt(
+        { ...user, session: session._id },  
+        { expiresIn: config.get('accessTokenTtl') } // 15 minutes
+    );
 
-     // create a refresh token
 
-    // return access & refresh tokens
 }
