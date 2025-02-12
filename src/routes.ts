1fbcf85dc27475c4bbaa62b7585ad01ab3cda349
zalacop <103320914+zalacop@users.noreply.github.com>
2025-02-12 16:15:25 +0100

    adds delete session route

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import { Express, Request, Response } from "express";
 import { createUserHandler } from "./controller/user.controller";
 import validate from "./middleware/validateResource";
 import { createUserSchema } from "./schema/user.schema";
-import { createUserSessionHandler, getUserSessionsHandler } from "./controller/session.controller";
+import { createUserSessionHandler, deleteSessionHandler, getUserSessionsHandler } from "./controller/session.controller";
 import { createSessionSchema } from "./schema/session.schema";
 import requireUser from "./middleware/requireUser";
 
@@ -13,7 +13,9 @@ function routes(app: Express) {
 
     app.post("/api/sessions", validate(createSessionSchema), createUserSessionHandler);
 
-    app.get("/api/sessions", requireUser, getUserSessionsHandler)
+    app.get("/api/sessions", requireUser, getUserSessionsHandler);
+
+    app.delete("/api/sessions", requireUser, deleteSessionHandler);
 }
 
 export default routes;
