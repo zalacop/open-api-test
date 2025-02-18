1e0a8d4afe0a6f787d67b410a8f4c9ab658d18d6
zalacop <103320914+zalacop@users.noreply.github.com>
2025-02-18 17:33:00 +0100

    adds reissue access token

--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -1,5 +1,9 @@
 import { FilterQuery, UpdateQuery } from "mongoose";
 import SessionModel, { SchemaDocument } from "../models/session.model";
+import { signJwt, verifyJwt } from "../utils/jwt.utils";
+import { get } from "lodash";
+import { findUser } from "./user.service";
+import config from "config";
 
 export async function createSession(userId: string, userAgent: string) {
     const session = await SessionModel.create({user: userId, userAgent});
@@ -13,4 +17,25 @@ export async function findSessions(query: FilterQuery<SchemaDocument>) {
 
 export async function updateSession(query: FilterQuery<SchemaDocument>, update: UpdateQuery<SchemaDocument>) {
     return SessionModel.updateOne(query, update);
+}
+
+export async function reIssueAccessToken({refreshToken}:{refreshToken: string}) {
+    const {decoded} = verifyJwt(refreshToken);
+
+    if(!decoded || !get(decoded, '_id')) return false;
+
+    const session = await SessionModel.findById(get(decoded, "_id"));
+
+    if(!session || !session.valid) return false;
+
+    const user = await findUser({ _id: session.user });
+
+    if(!user) return false;
+
+    const accessToken = signJwt(
+        { ...user, session: session._id },
+        { expiresIn: config.get("accessTokenTtl") }
+    );
+
+    return accessToken;
 }
