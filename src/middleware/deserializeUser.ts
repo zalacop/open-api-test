c8a8bc47c34a5b308e4c11b25b5d5192665ad7e6
zalacop <103320914+zalacop@users.noreply.github.com>
2025-02-18 17:33:24 +0100

    adds checks for expired and refresh token

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import { get } from "lodash";
 import { verifyJwt } from "../utils/jwt.utils";
+import { reIssueAccessToken } from "../service/session.service";
 
-const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
+const deserializeUser = async (req: Request, res: Response, next: NextFunction) => {
         const accessToken = get(req, "headers.authorization", "").replace(/^Bearer\s/, "");
+        const refreshToken = get(req, "headers.x-refresh");
 
         if(!accessToken) {
             return next();
@@ -15,6 +17,22 @@ const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
             res.locals.user = decoded;
             return next();
         }
+
+        if(expired && refreshToken) {
+            const newAccessToken = await reIssueAccessToken({ refreshToken: String(refreshToken) });
+
+            if(newAccessToken) {
+                res.setHeader('x-access-token', newAccessToken);
+            }
+
+            const result = verifyJwt(String(newAccessToken));
+
+            res.locals.user = result.decoded;
+            
+            return next();
+        }
+
+        return next();
 }
 
 export default deserializeUser;
