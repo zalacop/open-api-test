5de27834e61e03e4b87516dd6e040f3f5d8bc89f
zalacop <103320914+zalacop@users.noreply.github.com>
2025-02-24 17:53:32 +0100

    adds find product

--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -8,4 +8,10 @@ export async function createProduct(
     return ProductModel.create(input);
 }
 
+export async function findProduct(
+    query: FilterQuery<ProductDocument>,
+    options: QueryOptions = { lean: true }
+) {
+    return ProductModel.findOne(query, {}, options);
+}
 
