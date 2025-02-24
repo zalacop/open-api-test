256bdf66c6ca26e8bc4b8066da91a7e49f0971e2
zalacop <103320914+zalacop@users.noreply.github.com>
2025-02-24 17:16:36 +0100

    adds product model

---
+++ src/models/product.model.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import { UserDocument } from "./user.models";
+import { customAlphabet } from "nanoid";
+
+const nanoid = customAlphabet('abcdefghijklmnopqurstuvwxyz0123456789', 10);
+
+export interface ProductDocument extends mongoose.Document {
+    user: UserDocument['_id'];
+    title: string;
+    description: string;
+    price: number;
+    image: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const ProductSchema = new mongoose.Schema(
+    {
+        productId: {
+            type: String,
+            required: true,
+            unique: true,
+            default: () => `product_${nanoid()}`,
+        },
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        title: { type: String, required: true },
+        description: { type: String, required: true },
+        price: { type: Number, required: true },
+        image: { type: String, required: true },
+    }, 
+    {
+        timestamps: true
+    }
+);
+
+const ProductModel = mongoose.model<ProductDocument>("Product", ProductSchema);
+
+export default ProductModel;
