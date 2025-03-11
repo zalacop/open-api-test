df2ba8f59798a6915f7fb375d2276e05bc2b2c60
zalacop <103320914+zalacop@users.noreply.github.com>
2025-03-11 14:20:25 +0100

    adds create product handler

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from "express";
 import { createProductInput, updateProductInput } from "../schema/product.schema";
+import { createProduct } from "../service/product.service";
 
 export async function createProductHandler(
     req: Request<{}, {}, createProductInput["body"]>, 
     res: Response) {
+        const userId = res.locals.user._id;
 
+        const body = req.body;
+
+        const product = await createProduct({ ...body, user: userId });
+
+        return res.send(product);
 }
 
 export async function updateProductHandler(
