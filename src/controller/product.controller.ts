0cc2c0b76a658d9b88d9e880d6ec75d0e55e41e6
zalacop <103320914+zalacop@users.noreply.github.com>
2025-03-11 14:27:54 +0100

    adds get product handler

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { createProductInput, updateProductInput } from "../schema/product.schema";
-import { createProduct } from "../service/product.service";
+import { createProduct, findAndUpdateProduct, findProduct } from "../service/product.service";
 
 export async function createProductHandler(
     req: Request<{}, {}, createProductInput["body"]>, 
@@ -23,7 +23,24 @@ export async function updateProductHandler(
 export async function getProductHandler(
     req: Request<updateProductInput["params"]>,
     res: Response) {
-    
+        const userId = res.locals.user._id;
+
+        const productId = req.params.productId;
+        const update = req.body;
+
+        const product = await findProduct( { productId });
+
+        if(!product) {
+            return res.sendStatus(404);
+        }
+
+        if(product.user !== userId) {
+            return res.sendStatus(403);
+        }
+
+        const updatedProduct = await findAndUpdateProduct({ productId }, update, {new: true,});
+
+        return res.send(updatedProduct);
 }
 
 export async function deleteProductHandler(
