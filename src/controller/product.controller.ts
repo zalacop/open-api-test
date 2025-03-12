0f83c86a60f043813dbf3b22151620cc99e954b5
zalacop <103320914+zalacop@users.noreply.github.com>
2025-03-12 12:00:12 +0100

    adds delete product handler

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { createProductInput, updateProductInput } from "../schema/product.schema";
-import { createProduct, findAndUpdateProduct, findProduct } from "../service/product.service";
+import { createProduct, deleteProduct, findAndUpdateProduct, findProduct } from "../service/product.service";
 
 export async function createProductHandler(
     req: Request<{}, {}, createProductInput["body"]>, 
@@ -46,5 +46,21 @@ export async function getProductHandler(
 export async function deleteProductHandler(
     req: Request<updateProductInput["params"]>, 
     res: Response) {
-    
+        const userId = res.locals.user._id;
+
+        const productId = req.params.productId;
+
+        const product = await findProduct( { productId });
+
+        if(!product) {
+            return res.sendStatus(404);
+        }
+
+        if(product.user !== userId) {
+            return res.sendStatus(403);
+        }
+
+        await deleteProduct({ productId });
+
+        return res.sendStatus(200);
 }
