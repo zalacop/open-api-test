decdd1a85bacfb45ec19e3d937bb2a73dca218dd
zalacop <103320914+zalacop@users.noreply.github.com>
2025-03-13 10:09:30 +0100

    adds route to products

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,8 @@ import { createUserSchema } from "./schema/user.schema";
 import { createUserSessionHandler, deleteSessionHandler, getUserSessionsHandler } from "./controller/session.controller";
 import { createSessionSchema } from "./schema/session.schema";
 import requireUser from "./middleware/requireUser";
+import { createProductSchema } from "./schema/product.schema";
+import { createProductHandler } from "./controller/product.controller";
 
 function routes(app: Express) {
     app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
@@ -16,6 +18,8 @@ function routes(app: Express) {
     app.get("/api/sessions", requireUser, getUserSessionsHandler);
 
     app.delete("/api/sessions", requireUser, deleteSessionHandler);
+
+    app.post('/api/products', [requireUser, validate(createProductSchema)], createProductHandler)    
 }
 
 export default routes;
