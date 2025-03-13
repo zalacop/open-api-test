0fba94d3763fae3afcfcd47220b6970f7dc59260
zalacop <103320914+zalacop@users.noreply.github.com>
2025-03-13 10:17:04 +0100

    update routes

--- src/routes.ts
+++ src/routes.ts
@@ -23,9 +23,9 @@ function routes(app: Express) {
 
     app.put('/api/products', [requireUser, validate(updateProductSchema)], updateProductHandler);
 
-    app.get('/api/products', validate(getProductSchema), getProductHandler);
+    app.get('/api/products/:productId', validate(getProductSchema), getProductHandler);
 
-    app.delete('/api/products', [requireUser, validate(deleteProductSchema)], deleteProductHandler);
+    app.delete('/api/products/:productId', [requireUser, validate(deleteProductSchema)], deleteProductHandler);
 }
 
 export default routes;
