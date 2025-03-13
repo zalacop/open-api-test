a15092c502c93e8e505833d8cfa86c71d5cd30c8
zalacop <103320914+zalacop@users.noreply.github.com>
2025-03-13 10:22:08 +0100

    updated config default

--- config/default.ts
+++ config/default.ts
@@ -2,7 +2,10 @@ export default {
     port: 1337,
     dbUri: "mongodb://localhost:27017/open-api-test",
     saltWorkFactor: 10,
-    accessTokenTtl: '15m',
-    refreshTokenTtl: '1y',
-    
+    accessTokenTtl: "15m",
+    refreshTokenTtl: "1y",
+    accessTokenPrivateKey: ``,
+    accessTokenPublicKey: ``,
+    refreshTokenPrivateKey: ``,
+    refreshTokenPublicKey: ``,
 }
